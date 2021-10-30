668183c43400e5e04d34b9c93f45bbcc721204c6
MaximeMlt <90554393+MaximeMlt@users.noreply.github.com>
2021-10-30 12:11:37 +0200

    Update sort.ml

--- algo/sort/sort.ml
+++ algo/sort/sort.ml
@@ -26,3 +26,20 @@ let rec exporapide a n =
         let b = exporapide a (n/2) in
         if n mod 2 = 0 then b*b
         else a*b*b
+
+(*tester si une suite est croissante*)
+let rec croissant l = match l with
+    |[]-> true
+    |_::[] -> true
+    |e1::e2::q -> if e1 >= e2 then false else  croissant (e2::q)
+    
+(*dichotomie*)
+let dicho e t = 
+    let rec aux i j =
+        if i>j then false
+        else 
+            let m = ((i+j)/2) in
+            if e = t.(m)     then true
+            else if e > t.(m)    then aux (m+1) j 
+            else     aux i (m-1)
+        in aux 0 (Array.length t -1)
