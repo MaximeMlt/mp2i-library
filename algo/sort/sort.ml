2bb903b289e3173397bf64bc52e0c1ceea0fc462
MaximeMlt <90554393+MaximeMlt@users.noreply.github.com>
2021-10-31 21:05:05 +0100

    Update sort.ml

--- algo/sort/sort.ml
+++ algo/sort/sort.ml
@@ -43,3 +43,20 @@ let dicho e t =
             else if e > t.(m)    then aux (m+1) j 
             else     aux i (m-1)
         in aux 0 (Array.length t -1)
+
+(*tri fusion*)
+let rec split = function
+    | [] -> [], []
+    | [e] -> [e], []
+    | e1::e2::q -> let q1, q2 = split q in e1::q1, e2::q2
+
+let rec fusion l1 l2 = match l1, l2 with
+    | [], _ -> l2
+    | _, [] -> l1
+    | e1::q1, e2::q2 when e1 < e2 -> e1::fusion q1 l2
+    | e1::q1, e2::q2 -> e2::fusion l1 q2
+    
+let rec tri = function
+    | [] -> []
+    | [e] -> [e] 
+    | l -> let l1, l2 = split l in fusion (tri l1) (tri l2);;
