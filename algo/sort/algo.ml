c0eb35167f170a996af322c8e6c4cf5a975e7a3a
MaximeMlt <90554393+MaximeMlt@users.noreply.github.com>
2021-11-01 22:39:43 +0100

    Update algo.ml

--- algo/sort/algo.ml
+++ algo/sort/algo.ml
@@ -60,3 +60,15 @@ let rec tri = function
     | [] -> []
     | [e] -> [e] 
     | l -> let l1, l2 = split l in fusion (tri l1) (tri l2);;
+
+(*trichotomie*)
+let tricho t e = 
+    let rec aux i j =
+        if i > j then false
+        else let m1 = (2*i + j + 1)/3 in
+            let m2 = (i + 2*j + 2)/3 in
+            if t.(m1) = e || t.(m2) = e then true
+            else if e < t.(m1) then aux i (m1 - 1)
+            else if e < t.(m2) then aux (m1 + 1) (m2 - 1)
+            else aux (m2 + 1) j in
+    aux 0 (Array.length t - 1)
